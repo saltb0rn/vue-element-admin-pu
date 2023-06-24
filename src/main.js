d0a4c8ecb1033eab30f90a6e778c8d5fe0f30c88
Saltb0rn <asche34@outlook.com>
2023-06-24 11:15:22 +0800

    update

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import 'normalize.css/normalize.css' // a modern alternative to CSS resets
 
 import Element from 'element-ui'
 import './styles/element-variables.scss'
-
 import '@/styles/index.scss' // global css
 
 import App from './App'
